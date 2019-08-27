23d7c7795fb26e95cf22658fd4be9741c3ff2206
Brian Larson <manateeman24@hotmail.com>
2019-08-27 16:29:37 +0100

    code scaffolding

---
+++ SiteScrapers/Indeed/handler.py
@@ -0,0 +1,10 @@
+def uploadFindingsToDB(findings):
+    # uploads results from scraper to DB
+    return ;
+
+
+def searchGlassDoor():
+    str = 'do a thing'
+    GDFindings = []
+    uploadFindingsToDB(GDFindings)
+    return str
